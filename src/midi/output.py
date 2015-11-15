6a4412a50287f6dee0bd0b327e0b4b0c0de46f80
Roelof Ruis <roelof@branchonline.nl>
2015-11-15 15:14:11 +0100

    Added basic file for midi output

---
+++ src/midi/output.py
@@ -0,0 +1,6 @@
+# Part of the Soundsentences program
+# By Roelof Ruis <roelof.ruis@gmail.com>
+from midiutil.MidiFile import MIDIFile
+
+# Reference the documentation provided at
+# http://www.emergentmusics.org/mididutil-class-reference
